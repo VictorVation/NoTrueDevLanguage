d3ab8331450d7a8320396506ad6528c63cf9fc48
Victor Szeto <me@victorszeto.com>
2014-12-23 03:06:41 -0500

    simpler approach

---
+++ README.md
@@ -0,0 +1,4 @@
+#NoTrueDevLanguage
+Chrome extension that removes any mentions the phrase "only real dev language" from all Facebook comments. Good stuff is in [/ext/src/inject/inject.js](/ext/src/inject/inject.js).
+
+[Google Web Store](https://chrome.google.com/webstore/detail/no-true-dev-language/hgdkegkmjfmjnbcbgbpipphmcpepeped).
--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -1,27 +1,22 @@
 chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
-	if (document.readyState === "complete") {
-		clearInterval(readyStateCheckInterval);
-    var j = jQuery.noConflict();
-    var badWords = /node(\.js)? is the only|real dev(eloper)? lang(uage)?/i;
-    var killNode = function() {
-      j('.UFIImageBlockContent').each(function() {
-        if (badWords.test(this.textContent) == true) {
-          j(this).parents('.UFIComment').css('backgroundColor', '#EF5350').slideUp(500);
-        }
-      });
-    };
-    j('span:contains("more comments")').each(function() {
+  	if (document.readyState === "complete") {
+  		clearInterval(readyStateCheckInterval);
 
-      j(this).on('click', function() {
-        setTimeout(function() {
-          killNode();
-          console.log('clicked');
-        }, 2000);
-      });
-    });
-    killNode();
-	}
+      var j = jQuery.noConflict();
+      var badWords = /node(\.js)? is the only|real dev(eloper)? lang(uage)?/i;
+      var killNode = function() {
+        j('.UFIImageBlockContent').each(function() {
+          if (badWords.test(this.textContent) == true) {
+            j(this).parents('.UFIComment').css('backgroundColor', '#EF5350').slideUp(500);
+          }
+        });
+      };
+      setInterval(function() {
+        killNode();
+      }, 3500);
+
+  	}
 	}, 10);
 });
 
