e2af3ffb79d1f83cb9cd1b57d6d70e752594f48d
Victor Szeto <me@victorszeto.com>
2014-12-23 02:28:55 -0500

    kill nodejs after clicking 'view more comments'

--- ext/src/inject/inject.js
+++ ext/src/inject/inject.js
@@ -2,10 +2,9 @@ chrome.extension.sendMessage({}, function(response) {
 	var readyStateCheckInterval = setInterval(function() {
 	if (document.readyState === "complete") {
 		clearInterval(readyStateCheckInterval);
-
     var j = jQuery.noConflict();
     var badWords = /node(\.js)? is the only|real dev(eloper)? lang(uage)?/i;
-    var killNodejs = function() {
+    var killNode = function() {
       j('.UFIImageBlockContent').each(function() {
         if (badWords.test(this.textContent) == true) {
           j(this).parents('.UFIComment').css('backgroundColor', '#EF5350').slideUp(800);
@@ -13,8 +12,13 @@ chrome.extension.sendMessage({}, function(response) {
       });
     };
     j('span:contains("more comments")').each(function() {
-      $(this).on('click', killNodejs)
+      j(this).on('click', function(){
+        setTimeout(function() {
+          killNode();
+        }, 1000);
+      })
     })
+    killNode();
 	}
 	}, 10);
 });
